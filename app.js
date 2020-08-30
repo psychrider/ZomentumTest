86c243422bfce1ecdf287a92ccd53da97fd921c9
psychrider <satyamshivamsp@gmail.com>
2020-08-31 03:03:28 +0530

    touch ups

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express()
-var mongoose = require('mongoose');
-var bodyParser = require("body-parser");
-var Ticket = require('./models/ticket.js');
-var Show = require('./models/show.js');
-var methodOverride = require('method-override');
-const { findByIdAndDelete } = require('./models/ticket.js');
+var express        = require('express'),
+    app            = express(),
+    mongoose       = require('mongoose'),
+    bodyParser     = require("body-parser"),
+    Ticket         = require('./models/ticket.js'),
+    Show           = require('./models/show.js'),
+    methodOverride = require('method-override');
 
 
+// Connection to database
 mongoose.connect('mongodb://localhost:27017/new_DB_3', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -15,10 +15,13 @@ mongoose.connect('mongodb://localhost:27017/new_DB_3', {
 .then(() => console.log('Connected to DB!'))
 .catch(error => console.log(error.message));
 
+// for PUT and DELETE methods
 app.use(methodOverride("_method"));
+
 app.use(bodyParser.urlencoded({extended: true}));//don't ask why
 app.set("view engine","ejs");
 
+// index route to display all shows 
 app.get('/show',function(req,res){
     Show.find({},function(err,show){
         if(err){
@@ -29,7 +32,7 @@ app.get('/show',function(req,res){
         }
     })
 })
-
+//create route
 app.post('/show',function(req,res){
     var start = "10:00:00";
     Show.create({
@@ -44,32 +47,16 @@ app.post('/show',function(req,res){
         }
     })
 })
-//--------------------------------------------------------------
-// app.get('/show/new',function(req,res){
-//     res.render("new_show.ejs");
-// })
 
 //TICKET ROUTES---
 
-app.get('/ticket',function(req,res){
-    Ticket.find({},function(err,ticket){
-        res.send(ticket);
-    })    
-})
-
-//start:req.query.start
-app.get('/ticket/:id',function(req,res){
-    Ticket.findById(req.params.id,function(err,ticket){
-        //show user
-        res.send({ticket:ticket});
-    })
-})
-//NEW TICKET CREATION
+//An endpoint to book a ticket using a user’s name, phone number, and timings.
 app.post('/ticket',function(req,res){
+    // data hard-coded for testing
     var ud = {
-        name: "rbaterae",
-        phone: 99934343,
-        booking_time: "07:30:00"
+        name: "abcdefghj",
+        phone: 8834544944,
+        booking_time: "09:30:00"
     }
     Ticket.create({
         user_detail:ud,
@@ -91,23 +78,8 @@ app.post('/ticket',function(req,res){
     })
 })
 
-//SHOW FORM FOR NEW TICKET
-// app.get('/ticket/new',function(req,res){
-//     Show.find({},function(err,show){
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             console.log(show);
-//             res.render("new_ticket.ejs",{show:show});
-//         }
-//     })
-// })
-//EDIT
-app.get('/ticket/:id/edit',function(req,res){
-    res.send("id for ticket to be updated is taken form database and then redirected to update route")
-})
-//UPDATE ROUTE
+
+//An endpoint to update a ticket timing.
 app.put('/ticket/:id',function(req,res){
     Ticket.findByIdAndUpdate(req.params.id,{start: req.body.start},{new:true},function(err,updatedTicket){
         if(err){
@@ -119,7 +91,16 @@ app.put('/ticket/:id',function(req,res){
         }
     })
 })
-//DELETE ROUTE
+
+
+//An endpoint to view all the tickets for a particular time.
+app.get('/ticket',function(req,res){
+    Ticket.find({start:req.query.start},function(err,ticket){
+        res.send(ticket);
+    })    
+})
+
+//An endpoint to delete a particular ticket.
 app.delete('/ticket/:id',function(req,res){
     Ticket.findByIdAndDelete(req.params.id,function(err){
         if(err){
@@ -131,18 +112,15 @@ app.delete('/ticket/:id',function(req,res){
     })
 })
 
+// An endpoint to view the user’s details based on the ticket id.
 
-// var name = "shivam pandey";
-// var number = "9984473834";
-// var booktime = Date.now;
+app.get('/ticket/:id',function(req,res){
+    Ticket.findById(req.params.id,function(err,ticket){
+        //show user
+        res.send({ticket:ticket});
+    })
+})
 
-// Ticket.create({
-
-//     },function(){
-
-// })
-
-// })
 app.listen(3000,function(){
     console.log("server is litening");
 })
